6245fa75780e0f9adb4361180ad52477f2bd848d
RexKao <ww10282002@gmail.com>
2023-04-12 17:33:30 +0800

    homework 2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int endBuy = 0;
 
-        System.out.print("Please enter number of tickets: ");
-        int totalTickets = scanner.nextInt();
-        System.out.print("How many round-trip tickets: ");
-        int roundTickets = scanner.nextInt();
+        while (endBuy != -1) {
+            System.out.print("Please enter number of tickets: ");
+            int totalTickets = scanner.nextInt();
+            System.out.print("How many round-trip tickets: ");
+            int roundTickets = scanner.nextInt();
 
-        Ticket ticket = new Ticket(totalTickets, roundTickets);
-        ticket.print();
+            Ticket ticket = new Ticket(totalTickets, roundTickets);
+            ticket.print();
+
+            System.out.print("If you want to exit buying system, you can input -1: ");
+            endBuy = scanner.nextInt();
+        }
     }
 }
--- src/main/kotlin/com/train/TestKotlin.kt
+++ src/main/kotlin/com/train/TestKotlin.kt
@@ -4,14 +4,20 @@ import java.util.Scanner
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
+    var endBuy = 0
 
-    print("Please enter number of tickets: ")
-    val totalTickets = scanner.nextInt()
-    print("How many round-trip tickets: ")
-    val roundTickets = scanner.nextInt()
+    while (endBuy != -1) {
+        print("Please enter number of tickets: ")
+        val totalTickets = scanner.nextInt()
+        print("How many round-trip tickets: ")
+        val roundTickets = scanner.nextInt()
 
-    val ticket = TicketKotlin(totalTickets, roundTickets)
-    ticket.print()
+        val ticket = TicketKotlin(totalTickets, roundTickets)
+        ticket.print()
+
+        print("If you want to exit buying system, you can input -1: ")
+        endBuy = scanner.nextInt()
+    }
 }
 
 class TicketKotlin(var totalTickets :Int, var roundTickets :Int) {
